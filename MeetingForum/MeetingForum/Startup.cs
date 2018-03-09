ca86274b7a588eef6b22138e95a9b34e176699d1
Максим <maxshvec95@yandex.ua>
2018-03-09 22:10:10 +0200

    Commit proj

---
+++ MeetingForum/MeetingForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetingForum.Startup))]
+namespace MeetingForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
